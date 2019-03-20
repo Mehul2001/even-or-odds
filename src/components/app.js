29d2808135a974e4bbf06c512efb0217bf42445b
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-19 19:27:40 -0700

    Connect React Components to Redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
     render() {
+        console.log(this);
         return (
             <div>React App</div>
         )
     }
 }
+const mapStateToProps = state => {
+    console.log('state', state);
 
-export default App;
+    return { gamestarted: state.gamestarted };
+}
+
+const componentConnector = connect(mapStateToProps);
+export default componentConnector(App);
