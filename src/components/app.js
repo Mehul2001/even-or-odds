5441d531037884acf779670f6b4d62403a307d0d
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-20 17:27:55 -0700

    Track the correct guesses in the app

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,9 @@ import fetchState from '../reducers/fetchState';
 import DrawCard from './DrawCard';
 import { fetchNewDeck } from '../actions/deck';
 import Card from './Card';
+import Guess from './Guess';
+import GameState from './GameState';
+
 class App extends Component {
     startGame = () => {
         this.props.startGame();
@@ -30,6 +33,9 @@ class App extends Component {
                     this.props.gameStarted ? (
                         <div>
                             <h3>The game is on!</h3>
+                            <GameState />
+                            <br />
+                            <Guess />
                             <br />
                             <DrawCard />
                             <hr />
